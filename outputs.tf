d165949aeadd8a0965b8f06216744abd3e6daa5b
Immersive <105320165+immmersive@users.noreply.github.com>
2023-12-16 20:44:30 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "bucket_id" {
     value = "${aws_s3_bucket.s3_web.id}"
 }
 
+output "aws_lambda_function" {
+    value = aws_lambda_function.lambda.function_name
+}
+
 output "cloudfront_id" {
     value = "${aws_cloudfront_distribution.cloudfront_web.id}"
 }
